42e1ff71516b016bddd7d3cb69e71e349231a673
chee <chee@snaek.org>
2016-12-13 01:47:51 +0000

    refactor xevents to use object of funcs instead of big event monster

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const Window = require('./window')
 const listen = require('./srv')
 
 const commands = new events.EventEmitter
-const xevents = new events.EventEmitter
 
 const name = 'wm'
 
@@ -19,8 +18,6 @@ const NOPE = 0
 
 process.env.PATH = `${dirname(process.argv[1])}/bin:${process.env.PATH}`
 
-let attributes
-let start
 let X
 let workspaces
 let currentWorkspace = null
@@ -68,6 +65,81 @@ function makeWorkspaces(size) {
   return workspaces
 }
 
+// todo: move this into its own file
+// X Events
+const Event = {
+  start: null,
+  attributes: null,
+  KeyPress({buttons, keycode}) {
+    keybindings.forEach(binding => {
+      if (buttons == binding.buttons && keycode == binding.keycode) {
+        exec(binding.cmd, console.log)
+      }
+    })
+  },
+  ButtonPress(event) {
+    const {child} = event
+    X.RaiseWindow(child)
+    currentWorkspace.currentWindow = Window.create(child)
+    Window.focus(currentWorkspace.currentWindow)
+    if (!Workspace.contains(currentWorkspace, currentWorkspace.currentWindow)) {
+      Workspace.addWindow(currentWorkspace, currentWorkspace.currentWindow)
+    }
+    X.GetGeometry(child, (error, attr) => {
+      this.start = event
+      this.attributes = attr
+    })
+  },
+  MotionNotify({rootx, rooty}) {
+    if (!this.start) return
+    const {start, attributes} = this
+    const xdiff = rootx - start.rootx
+    const ydiff = rooty - start.rooty
+    start.keycode == 1 && start.buttons == keys.buttons.M && X.MoveWindow(
+      start.child,
+      attributes.xPos + (start.keycode == 1 ? xdiff : 0),
+      attributes.yPos + (start.keycode == 1 ? ydiff : 0)
+    )
+    start.keycode == 3 && X.ResizeWindow(
+      start.child,
+      Math.max(1, attributes.width + (start.keycode == 3 ? xdiff : 0)),
+      Math.max(1, attributes.height + (start.keycode == 3 ? ydiff : 0))
+    )
+  },
+  ButtonRelease() {
+    this.attributes = this.start = null
+  },
+  MapRequest(event) {
+    X.GetWindowAttributes(event.wid, (error, attributes) => {
+      if (error) return console.error(error)
+      if (attributes[8]) {
+        return X.MapWindow(event.wid)
+      }
+    })
+    X.ChangeWindowAttributes(
+      event.wid,
+      {eventMask: x11.eventMask.EnterWindow}
+    )
+    const window = Window.create(event.wid)
+    Workspace.addWindow(currentWorkspace, window)
+  },
+  FocusIn(event) {
+    currentWorkspace.currentWindow = Window.create(event.wid)
+    Window.focus(currentWorkspace.currentWindow)
+  },
+  EnterNotify(event) {
+    currentWorkspace.currentWindow = Window.create(event.wid)
+    Window.focus(currentWorkspace.currentWindow)
+  },
+  ConfigureRequest({wid, width, height}) {
+    X.ResizeWindow(wid, width, height)
+  },
+  DestroyNotify({wid}) {
+    const window = Window.create(wid)
+    workspaces.forEach(workspace => Workspace.removeWindow(workspace, window))
+  }
+}
+
 function createClient() {
   x11.createClient((error, display) => {
     screen = display.screen[0]
@@ -92,70 +164,15 @@ function createClient() {
       | x11.eventMask.FocusChange
     }, console.error)
   })
-  .on('event', event => xevents.emit(event.name, event))
+  .on('event', event => {
+    Event[event.name]
+    ? Event[event.name](event)
+    : console.warn(`unhandled event: ${event.name}`)
+  })
   .on('error', console.error)
 }
 
-function focus(event) {
-  currentWorkspace.currentWindow = Window.create(event.wid)
-  Window.focus(currentWorkspace.currentWindow)
-}
-
-// X Events
-xevents.on('KeyPress', event => {
-  keybindings.forEach(binding => {
-    if (event.buttons == binding.buttons && event.keycode == binding.keycode) {
-      exec(binding.cmd, console.log)
-    }
-  })
-}).on('ButtonPress', event => {
-  const child = event.child
-  X.RaiseWindow(child)
-  currentWorkspace.currentWindow = Window.create(child)
-  Window.focus(currentWorkspace.currentWindow)
-  if (!Workspace.contains(currentWorkspace, currentWorkspace.currentWindow)) {
-    Workspace.addWindow(currentWorkspace, currentWorkspace.currentWindow)
-  }
-  X.GetGeometry(child, (error, attr) => {
-    start = event
-    attributes = attr
-  })
-}).on('MotionNotify', event => {
-  if (!start) return
-  const xdiff = event.rootx - start.rootx
-  const ydiff = event.rooty - start.rooty
-  start.keycode == 1 && start.buttons == keys.buttons.M && X.MoveWindow(
-    start.child,
-    attributes.xPos + (start.keycode == 1 ? xdiff : 0),
-    attributes.yPos + (start.keycode == 1 ? ydiff : 0)
-  )
-  start.keycode == 3 && X.ResizeWindow(
-    start.child,
-    Math.max(1, attributes.width + (start.keycode == 3 ? xdiff : 0)),
-    Math.max(1, attributes.height + (start.keycode == 3 ? ydiff : 0))
-  )
-}).on('ButtonRelease', () => start = null)
-.on('MapRequest', event => {
-  X.GetWindowAttributes(event.wid, (error, attributes) => {
-    if (error) return console.error(error)
-    if (attributes[8]) {
-      return X.MapWindow(event.wid)
-    }
-  })
-  X.ChangeWindowAttributes(
-    event.wid,
-    {eventMask: x11.eventMask.EnterWindow}
-  )
-  const window = Window.create(event.wid)
-  Workspace.addWindow(currentWorkspace, window)
-}).on('FocusIn', focus).on('EnterNotify', focus)
-.on('ConfigureRequest', event => {
-  X.ResizeWindow(event.wid, event.width, event.height)
-}).on('DestroyNotify', event => {
-  const window = Window.create(event.wid)
-  workspaces.forEach(workspace => Workspace.removeWindow(workspace, window))
-})
-
+// todo: move this into its own file
 const Command = {
   workspace: {
     switch(id) {
@@ -176,8 +193,8 @@ const Command = {
       const window = currentWorkspace.currentWindow
       const target = workspaces[constrainNumber(id - 1, workspaces.length)]
       Workspace.removeWindow(currentWorkspace, window)
-      Window.hide(window)
       Workspace.addWindow(target, window)
+      Window.hide(window)
       currentWorkspace.currentWindow = null
     },
     tile(direction) {

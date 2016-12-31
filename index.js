e813c7a1b283ffdab5e83a30d70b447d7abc79d4
chee <chee@snaek.org>
2016-12-31 17:38:00 +0000

    setup new workspaces when reloading

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const events = require('events')
 const x11 = require('x11')
 
 const Command = require('./src/command')
-const Workspace = require('./src/workspace')
 const Event = require('./src/event')
 const listen = require('./src/srv')
-const bind = require('./src/bind')
+const configure = require('./src/configure')
 
 const name = require('./lib/name')
 
@@ -17,20 +16,11 @@ process.env.PATH = `${dirname(process.argv[1])}/bin:${process.env.PATH}`
 // globals are: X, screen, root, keybindings, workspaces & currentWorkspace
 function createClient() {
   x11.createClient((error, display) => {
-    const configuration = require('rc')(name)
-
     global.screen = display.screen[0]
     global.root = global.screen.root
     global.X = display.client
-
-    global.workspaces = makeWorkspaces(
-      configuration.settings && configuration.settings.workspaces
-      ? configuration.settings.workspaces
-      : 5
-    )
+    configure()
     global.currentWorkspace = global.workspaces[0]
-
-    bind()
   })
   .on('event', event => {
     Event[event.name]
@@ -38,14 +28,6 @@ function createClient() {
     : console.warn(`unhandled event: ${event.name}`)
   })
   .on('error', console.error)
-}
-
-function makeWorkspaces(size) {
-  const workspaces = []
-  for (let id = 0; id < size; id++) {
-    workspaces.push(Workspace.create(id))
-  }
-  return workspaces
 }
 
 emitter.on('cmd', ({target, method, message}) => (
---
+++ src/configure.js
@@ -0,0 +1,67 @@
+const {setupKeybindings} = require('./util')
+const keys = require('../lib/keys')
+const Workspace = require('./src/workspace')
+const x11 = require('x11')
+
+const ASYNC = 1
+const NOPE = 0
+
+function grabKeys(X, bindings) {
+  bindings.forEach(binding => {
+    X.GrabKey(global.root, true,
+      binding.buttons,
+      binding.keycode,
+      ASYNC,
+      ASYNC
+    )
+  })
+}
+
+// todo: make this key configurable
+function grabButtons(X) {
+  X.GrabButton(
+    global.root, true, x11.eventMask.ButtonPress | x11.eventMask.ButtonRelease | x11.eventMask.PointerMotion,
+    ASYNC, ASYNC, NOPE, NOPE, 1, keys.buttons.M
+  )
+  X.GrabButton(
+    global.root, true, x11.eventMask.ButtonPress | x11.eventMask.ButtonRelease | x11.eventMask.PointerMotion,
+    ASYNC, ASYNC, NOPE, NOPE, 3, keys.buttons.M
+  )
+}
+
+
+function makeWorkspaces(size) {
+  const workspaces = []
+  for (let id = 0; id < size; id++) {
+    global.workspaces && global.workspaces[id]
+    ? workspaces.push(global.workspaces[id])
+    : workspaces.push(Workspace.create(id))
+  }
+  return workspaces
+}
+
+module.exports = function() {
+  const configuration = require('../lib/configuration')
+
+  // todo: ungrab old keys
+  global.keybindings = setupKeybindings(configuration.keybindings)
+
+  grabKeys(global.X, global.keybindings)
+  grabButtons(global.X)
+
+  global.workspaces = makeWorkspaces(
+    configuration.settings && configuration.settings.workspaces
+    ? configuration.settings.workspaces
+    : 5
+  )
+
+  global.X.ChangeWindowAttributes(global.root, {
+    eventMask: x11.eventMask.SubstructureNotify
+    | x11.eventMask.SubstructureRedirect
+    | x11.eventMask.ResizeRedirect
+    | x11.eventMask.Exposure
+    | x11.eventMask.MapRequest
+    | x11.eventMask.EnterWindow
+    | x11.eventMask.FocusChange
+  }, console.error)
+}
